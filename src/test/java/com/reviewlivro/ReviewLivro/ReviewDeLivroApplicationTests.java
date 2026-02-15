e8fb8e78276b47d3f2c816395516920ddad07d89
Carlos Motta <chmottaa@gmail.com>
2026-02-15 17:26:36 -0300

    Primeiro commit

---
+++ src/test/java/com/reviewlivro/ReviewLivro/ReviewDeLivroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reviewlivro.ReviewLivro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReviewDeLivroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
